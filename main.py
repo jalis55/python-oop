2f03d8df3a7457eb21b4f1a7457a4c1ee1886a7f
jalis55 <jalismahamud2055@gmail.com>
2022-07-26 16:02:20 +0600

    designed a class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import csv
 class Items:
     pay_rate=0.8 #discount rate 20%
     all_items=[]
@@ -18,37 +18,23 @@ class Items:
     
     def apply_discount(self):
         self.price=self.price * self.pay_rate
+    @classmethod
+    def csv_data_reader(cls):
+        with open('items.csv','r') as f:
+            reader=csv.DictReader(f)
+            items=list(reader)
+        for item in items:
+            Items(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                qty=int(item.get('qty'))
+            )
+
     def __repr__(self) -> str:
         return f"Items({self.name},{self.price},{self.qty})"
 
-# item1=Items("phone",100,5)
 
-# print(item1.name,item1.price,item1.qty)
 
-# item2=Items("Laptop",1000,2)
-# print(item2.name,item2.price,item2.qty)
-
-#view attributes and methods of class and instances
-# print(item1.__dict__)
-# print(Items.__dict__)
-
-# print(item1.get_total_price())
-# item1.apply_discount()
-# print(item1.get_total_price())
-
-# item3=Items("mac",1000,1)
-# item3.pay_rate=0.7
-# item3.apply_discount()
-# print(item3.get_total_price())
-
-item1 = Items("Phone", 100, 1)
-item2 = Items("Laptop", 1000, 3)
-item3 = Items("Cable", 10, 5)
-item4 = Items("Mouse", 50, 5)
-item5 = Items("Keyboard", 75, 5)
-
-print(Items.all_items)
-
-item5.qty=item5.qty-1
+Items.csv_data_reader()
 
 print(Items.all_items)
